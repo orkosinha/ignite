e7250a8ee02f92fb120c8f6a9e54b6e95d421fce
Orko Sinha <os92@cornell.edu>
2019-06-16 02:16:20 -0400

    Add documentation

--- server/src/schema.js
+++ server/src/schema.js
@@ -2,7 +2,14 @@ const { gql } = require('apollo-server');
 
 const typeDefs = gql`
 type Query{
+    """
+    Gets a random event from the API
+    """
     randomEvent: Event!
+    """
+    Searches an event based on a type, participant, max_price, and accessibility parameter.
+    Returns an event array.
+    """
     searchEvent(
         type: [String]
         participants: [String]
@@ -12,10 +19,25 @@ type Query{
 }
 
 type Event {
+    """
+    The name of the activity
+    """
     activity: String
+    """
+    Can be "easy", "medium", or "hard"
+    """
     accessibility: String
+    """
+    Can be one of the 8 types of activites
+    """
     type: String
+    """
+    Can be "solo" or "group"
+    """
     participants: String
+    """
+    Can be "free", "$". "$$", or "$$$"
+    """
     price: String
 }
 `;
